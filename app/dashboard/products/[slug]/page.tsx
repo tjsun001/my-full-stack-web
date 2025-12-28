13e565662ccf3b4a414300f20ec437a3ba552f2a
thurman sanders <thurman.sanders@yahoo.com>
2025-12-28 11:49:18 -0500

    All changes for Vercel deployment

--- app/dashboard/products/[slug]/page.tsx
+++ app/dashboard/products/[slug]/page.tsx
@@ -1,28 +1,29 @@
 import api from "@/lib/axios";
 import { TProduct } from "@/types";
 
-type Props = {
-  params: Promise<{
-    slug: string;
-  }>;
+type ParamsProps = {
+  params: Promise<{ slug: string }>;
 };
 
-const Page = async ({ params }: Props) => {
-  console.log(params);
+export default async function Page({ params }: ParamsProps) {
+  // Unwrap the promise
   const { slug } = await params;
 
   try {
+    // Fetch product data
     const { data }: { data: TProduct } = await api.get(
       `/api/v1/products/${slug}`,
     );
-    return "product detail ==> " + JSON.stringify(data, null, 2);
-  } catch (error: any) {
-    const statusCode = error.response.status;
-    if (statusCode == 404) {
-      return `product [${slug}] not found`;
-    }
-    return "failed to load products";
-  }
-};
 
-export default Page;
+    // Render the product details (example: JSON)
+    return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  } catch (error: any) {
+    const statusCode = error?.response?.status;
+
+    if (statusCode === 404) {
+      return <p>Product &quot;{slug}&quot; not found</p>;
+    }
+
+    return <p>Failed to load product</p>;
+  }
+}
